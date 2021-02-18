d02741a8657f5d72094cc926cd10d2813a6eec06
glaucia86 <glaucia_lemos86@hotmail.com>
2021-02-18 20:15:23 -0300

    Incluindo os exemplos do módulo 02 aulas 08 e 10

---
+++ modulo-02/10-aula-tipo-boolean/booleanType.ts
@@ -0,0 +1,8 @@
+/**
+ * arquivo: booleanType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
+ * data: 17/02/2021
+ * author: Glaucia Lemos <@glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
