dffd286e88c2b4bdf09a7c4799ad03bcbec9c48f
glaucia86 <glaucia_lemos86@hotmail.com>
2021-02-20 23:34:19 -0300

    Incluindo a pasta para a demo 12

---
+++ modulo-02/12-aula-number-bigint/numberBigint.ts
@@ -0,0 +1,14 @@
+/**
+ * arquivo: numberBigint.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo number e bigint'
+ * data: 17/02/2021
+ * author: Glaucia Lemos <@glaucia_lemos86>
+ * doc referência: <number> https://www.typescriptlang.org/docs/handbook/basic-types.html#number
+ * doc referência: <bigint> https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-2.html#bigint
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
+
+// ==> Exemplos: number
+
+
+// ==> Exemplos: bigint
