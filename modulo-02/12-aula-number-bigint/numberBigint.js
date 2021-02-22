be785837267be03ab3f7e8a69040d8d06a39bb7e
glaucia86 <glaucia_lemos86@hotmail.com>
2021-02-21 21:16:22 -0300

    Desenvolvimento da demo - 12.

--- modulo-02/12-aula-number-bigint/numberBigint.js
+++ modulo-02/12-aula-number-bigint/numberBigint.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * arquivo: numberBigint.ts
  * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo number e bigint'
@@ -7,26 +8,21 @@
  * doc referência: <bigint> https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-2.html#bigint
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
-
 // ==> Exemplos: number
-
-let num1: number = 23.0;
-let num2: number = 0x78CF;
-let num3: number = 0o577;
-let num4: number = 0b110001;
-
+let num1 = 23.0;
+let num2 = 0x78CF;
+let num3 = 0o577;
+let num4 = 0b110001;
 console.log('Number - Ponto Flutuante...: ', num1);
 console.log('Number - Hexadecimal...: ', num2);
 console.log('Number - Octal...: ', num3);
 console.log('Number - Binário...: ', num4);
-
 // ==> Exemplos: bigint
-
-let big1: bigint = 9007199254740991n;
-let big2: bigint = 0b100000000000000000000000000000000000000000000000000011n;;
-let big3: bigint = 0x20000000000003n;
-let big4: bigint = 0o400000000000000003n;
-
+let big1 = 9007199254740991n;
+let big2 = 9007199254740995n;
+;
+let big3 = 0x20000000000003n;
+let big4 = 9007199254740995n;
 console.log('Bigint...: ', big1);
 console.log('Bigint - Binário...: ', big2);
 console.log('Bigint - Hexadecimal...: ', big3);
