be56e2251ca3ee55a5502356c37a078e54fefe21
glaucia86 <glaucia_lemos86@hotmail.com>
2021-03-14 23:40:26 -0300

    Motando as demos para aula 14.

--- modulo-02/14-aula-tipo-string/stringType.ts
+++ modulo-02/14-aula-tipo-string/stringType.ts
@@ -1,8 +1,13 @@
 /**
- * arquivo: booleanType.ts
- * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
- * data: 17/02/2021
+ * arquivo: stringType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo String'
+ * data: 14/03/2021
  * author: Glaucia Lemos <@glaucia_lemos86>
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
+
+// ==> Exemplo 01 - Single Quotes
+// ==> Exemplo 02 - Double Quotes
+// ==> Exemplo 03 - Back Ticks
+
