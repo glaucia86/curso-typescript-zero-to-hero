5f7818b4f03d56cbf9f57f336959affe1a164e69
glaucia86 <glaucia_lemos86@hotmail.com>
2021-04-05 00:13:25 -0300

    Desenvolvimento da aula - 16

--- modulo-02/16-aula-tipo-array/arrayType.js
+++ modulo-02/16-aula-tipo-array/arrayType.js
@@ -7,3 +7,31 @@
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#array
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
+// ==> Exemplo 01 - Uso de Colchetes:
+let frutas = ['🍍', '🍊', '🍎', '🍉', '🥭'];
+console.log(frutas[2]);
+// ==> Exemplo 02 - Array Object (Objeto Array):
+let frutas1 = ['🍍', '🍊', '🍎', '🍉', '🥭'];
+console.log(frutas1[3]);
+// ==> Exemplo 03 - Adicionando mais strings com método 'push'
+let idiomas = ['Português', 'Inglês', 'Espanhol', 'Francês'];
+console.log(idiomas);
+idiomas.push('Mandarim');
+console.log(idiomas);
+idiomas.push('Italiano');
+console.log(idiomas);
+// ==> Exemplo 04 - Identificar tamanho do array - método 'length'
+let idiomas1 = ['Português', 'Inglês', 'Espanhol', 'Francês'];
+console.log(idiomas1.length);
+// ==> Exemplo 05 - Exemplo de Array com Spread Operator
+let listaNumeros = [0, 1, 2, 3, 4, 5];
+listaNumeros = [...listaNumeros, 6, 7, 8, 9, 10];
+console.log(listaNumeros);
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+let linguagensArray = new Array('JavaScript', 'Python', 'PHP', 'C#');
+function funcaoLinguagens(linguagens) {
+    for (let i = 0; i < linguagens.length; i++) {
+        console.log(linguagensArray[i]);
+    }
+}
+funcaoLinguagens(linguagensArray);
--- modulo-02/16-aula-tipo-array/arrayType.ts
+++ modulo-02/16-aula-tipo-array/arrayType.ts
@@ -7,3 +7,45 @@
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
 
+// ==> Exemplo 01 - Uso de Colchetes:
+
+let frutas: string[] = ['🍍', '🍊', '🍎', '🍉', '🥭'];
+console.log(frutas[2]);
+
+// ==> Exemplo 02 - Array Object (Objeto Array):
+
+let frutas1: Array<string> = ['🍍', '🍊', '🍎', '🍉', '🥭'];
+console.log(frutas1[3]);
+
+// ==> Exemplo 03 - Adicionando mais strings com método 'push'
+
+let idiomas: Array<string> = ['Português', 'Inglês', 'Espanhol', 'Francês'];
+console.log(idiomas);
+idiomas.push('Mandarim');
+console.log(idiomas);
+idiomas.push('Italiano');
+console.log(idiomas);
+
+// ==> Exemplo 04 - Identificar tamanho do array - método 'length'
+
+let idiomas1: Array<string> = ['Português', 'Inglês', 'Espanhol', 'Francês'];
+console.log(idiomas1.length);
+
+// ==> Exemplo 05 - Exemplo de Array com Spread Operator
+
+let listaNumeros = [0, 1, 2, 3, 4, 5];
+listaNumeros = [...listaNumeros, 6, 7, 8, 9, 10];
+
+console.log(listaNumeros);
+
+// ==> Exemplo 06 - Exemplo de Array com laço de iteração
+
+let linguagensArray:string[] = new Array('JavaScript', 'Python', 'PHP', 'C#');
+
+function funcaoLinguagens(linguagens:string[]) {
+    for (let i = 0; i < linguagens.length; i++) {
+        console.log(linguagensArray[i]); 
+    }
+}
+
+funcaoLinguagens(linguagensArray);
