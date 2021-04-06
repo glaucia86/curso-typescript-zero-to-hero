788cd0d8256fa3af3a2c3be494064e882ee068a0
glaucia86 <glaucia_lemos86@hotmail.com>
2021-04-05 22:02:08 -0300

    Criando folder para a aula 18.

---
+++ modulo-02/18-aula-tipo-tuple/tupleType.ts
@@ -0,0 +1,22 @@
+/**
+ * arquivo: tupleType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Tuple'
+ * data: 28/03/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#tuple
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
+
+// ==> Exemplo 01 - Uso simples de Tuplas em TypeScript
+
+// ==> Exemplo 02 - Acessando o valor da Tupla
+
+// ==> Exemplo 03 - Outra forma de usar Tuplas em TypeScrit (com labels)
+
+// ==> Exemplo 04 - Usando Tuplas com Spread Operator
+
+//==> Exemplo 05 - Lista Heterogênea de Tupla:
+
+// ==> Exemplo 06 - Uso de função com Tuplas
+
+// ==> Exemplo 07 - Labeled Tuples com Spread Operator numa função
