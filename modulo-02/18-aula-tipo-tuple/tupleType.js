400f9cebb8f004ad7f28fb0dd4e9bdcfd717a2be
glaucia86 <glaucia_lemos86@hotmail.com>
2021-04-06 18:08:41 -0300

    Desenvolvimento da aula - 18

--- modulo-02/18-aula-tipo-tuple/tupleType.js
+++ modulo-02/18-aula-tipo-tuple/tupleType.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * arquivo: tupleType.ts
  * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Tuple'
@@ -6,56 +7,32 @@
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#tuple
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
-
 // ==> Exemplo 01 - Uso simples de Tuplas em TypeScript
-
-let pessoa: [string, string, number];
+let pessoa;
 pessoa = ['Glaucia Lemos', 'Cloud Advocate JavaScript', 34];
 // pessoa = [34, 'Cloud Advocate JavaScript', 'Glaucia Lemos'];
-
 console.log(pessoa);
-
 // ==> Exemplo 02 - Acessando o valor da Tupla
-
-let pessoa1: [string, string, number];
+let pessoa1;
 pessoa1 = ['Glaucia Lemos', 'Cloud Advocate JavaScript', 34];
-
 console.log(pessoa1[1]);
-
 // ==> Exemplo 03 - Outra forma de usar Tuplas em TypeScrit (com labels)
-let pessoa2: [nome: string, posicao: string, idade: number] = ['Glaucia Lemos', 'Cloud Advocate JavaScript', 34];
-
+let pessoa2 = ['Glaucia Lemos', 'Cloud Advocate JavaScript', 34];
 console.log(pessoa2);
-
-
 // ==> Exemplo 04 - Usando Tuplas com Spread Operator
-
-let listaFrutas: [string, ...string[]] = ['🍍', '🍊', '🍎', '🍉', '🥭'];
+let listaFrutas = ['🍍', '🍊', '🍎', '🍉', '🥭'];
 console.log(...listaFrutas);
-
 //==> Exemplo 05 - Lista Heterogênea de Tupla:
-
-let listaFrutas2: [number, boolean, ...string[]] = [5, true, ...listaFrutas];
+let listaFrutas2 = [5, true, ...listaFrutas];
 console.log(listaFrutas2);
-
 // ==> Exemplo 06 - Uso de função com Tuplas
-
-function listarPessoas(nomes: string[], idades: number[]) {
+function listarPessoas(nomes, idades) {
     return [...nomes, ...idades];
 }
-
 let resultado = listarPessoas(['Glaucia', 'Jurema'], [34, 68]);
-console.log(resultado)
-
-// ==> Exemplo 07 - Labeled Tuples com Spread Operator numa função
-
-type Nome = 
-        | [primeiroNome: string, sobrenome: string]
-        | [primeiroNome: string, nomeMeio: string, sobrenome: string]
-
-function criarPessoa(...nome: Nome) {
+console.log(resultado);
+function criarPessoa(...nome) {
     return [...nome];
 }
-
 console.log(criarPessoa('Glaucia', 'Lemos'));
 console.log(criarPessoa('Glaucia', 'de Souza', 'Lemos'));
