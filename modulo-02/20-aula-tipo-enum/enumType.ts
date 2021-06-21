c848bc9b6f8d29bb30ebefda33af552dcee3963e
glaucia86 <glaucia_lemos86@hotmail.com>
2021-06-20 23:12:18 -0300

    Criando a pasta para os exercícios e demos sobre 'Enums'

---
+++ modulo-02/20-aula-tipo-enum/enumType.ts
@@ -0,0 +1,8 @@
+/**
+ * arquivo: enumType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Tuple'
+ * data: 20/06/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#enum
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
