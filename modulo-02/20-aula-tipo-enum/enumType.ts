58ed524632e57fab238adc3eab1bfef7de2f03ab
glaucia86 <glaucia_lemos86@hotmail.com>
2021-06-21 23:57:58 -0300

    Preparando as demos da aula 20

--- modulo-02/20-aula-tipo-enum/enumType.ts
+++ modulo-02/20-aula-tipo-enum/enumType.ts
@@ -6,3 +6,11 @@
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#enum
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
+
+// ==> Exemplo 01: Numeric Enums (Enums Numérico)
+
+// ==> Exemplo 02: String Enums
+
+// ==> Exemplo 03: Como podemos acessar um valor de um Enum com uma chave: (usando o const)
+
+// ==> Exemplo 04: Quando usar enum?!
