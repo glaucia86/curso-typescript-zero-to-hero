cc7063713150983db241d560fe5c87648270b2e5
glaucia86 <glaucia_lemos86@hotmail.com>
2021-07-29 19:41:37 -0300

    exemplos aula - 22

--- modulo-02/22-aula-tipo-any/anyType.ts
+++ modulo-02/22-aula-tipo-any/anyType.ts
@@ -7,7 +7,24 @@
  */
 
 // ==> Exemplo 01: Tipo Any
+const a: any = 34;
+const b: any = ['Glaucia'];
+
+const result = a + b;
+console.log(result);
 
 // ==> Exemplo 02: Quando o tipo 'any' é inferido implícitamente
+/*let frase;
+frase = 'Oi, pessoal! Tudo bem?';
+
+console.log(frase);*/
 
 // ==> Exemplo 03: Quando devemos usar o tipo any?!
+
+const formulario: { [campoFomulario: string]: any } = {
+  nome: 'Glaucia',
+  sobrenome: 'Lemos',
+  idade: 34,
+};
+
+console.log(formulario);
