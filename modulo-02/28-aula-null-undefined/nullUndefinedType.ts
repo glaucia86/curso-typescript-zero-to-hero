a4c0e2e55b4ec96994ae883edbe7a8efc5000768
glaucia86 <glaucia_lemos86@hotmail.com>
2021-08-22 19:53:21 -0300

    criando os arquivos para aula-28

---
+++ modulo-02/28-aula-null-undefined/nullUndefinedType.ts
@@ -0,0 +1,7 @@
+/**
+ * arquivo: nullUndefinedType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre os tipos 'null' e 'undefined'
+ * data: 27/08/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html
+ */
