18ce8214627e9a44acd2776c8082dd7db4fbc854
glaucia86 <glaucia_lemos86@hotmail.com>
2021-08-26 19:52:06 -0300

    criando os arquivos para aula-26

---
+++ modulo-02/26-aula-tipo-void/voidType.ts
@@ -0,0 +1,7 @@
+/**
+ * arquivo: voidType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'unknown'
+ * data: 27/08/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#void
+ */
