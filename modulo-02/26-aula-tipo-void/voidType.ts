d67f2c2d6dff3c496f13c91c9c9d21e6bb9bb643
glaucia86 <glaucia_lemos86@hotmail.com>
2021-08-27 17:24:04 -0300

    Desenvolvimento da aula 26

--- modulo-02/26-aula-tipo-void/voidType.ts
+++ modulo-02/26-aula-tipo-void/voidType.ts
@@ -1,7 +1,33 @@
 /**
  * arquivo: voidType.ts
- * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'unknown'
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'void'
  * data: 27/08/2021
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#void
  */
+
+// ==> Exemplo 01 - funções:
+
+function logError(errorMessage: string): void {
+  console.log(errorMessage);
+  // return errorMessage;
+}
+
+logError('Required field - Name!');
+
+// ==> Exemplo 02 - funções:
+
+const logErrorExample2 = (errorMessage: string): void => {
+  console.log(errorMessage);
+};
+
+logErrorExample2('Required field - Surname');
+
+// ==> Exemplo void: variáveis
+
+let variavelExemploVoid: void;
+// variavelExemploVoid = 1;
+variavelExemploVoid = null;
+variavelExemploVoid = undefined;
+
+console.log(variavelExemploVoid);
