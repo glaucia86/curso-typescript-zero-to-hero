051bcabc29d1a26f5fade4b201e2f0818ea429cd
glaucia86 <glaucia_lemos86@hotmail.com>
2021-09-14 14:11:41 -0300

    atualização do exemplo da aula 28

--- modulo-02/28-aula-null-undefined/nullUndefinedType.ts
+++ modulo-02/28-aula-null-undefined/nullUndefinedType.ts
@@ -1,7 +1,27 @@
+/* eslint-disable prefer-const */
 /**
  * arquivo: nullUndefinedType.ts
  * descrição: arquivo responsável por ensinar conceitos básicos sobre os tipos 'null' e 'undefined'
- * data: 27/08/2021
+ * data: 14/09/2021
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html
  */
+
+// ==> Exemplo 01: Null
+
+let variavelTesteNull = null;
+
+console.log(variavelTesteNull);
+console.log(typeof variavelTesteNull);
+
+// ==> Exemplo 02: Undefined
+
+let variavelTesteUndefined;
+
+console.log(variavelTesteUndefined);
+console.log(typeof variavelTesteUndefined);
+
+// ==> Diferenças e Similaridades: Null vs Undefined
+
+console.log('Exemplo 01:', null == undefined);
+console.log('Exemplo 02:', null === undefined);
