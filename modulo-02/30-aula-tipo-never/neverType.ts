48ae902d23fbdc59e85fd95ec2eb46667529d028
glaucia86 <glaucia_lemos86@hotmail.com>
2021-10-04 22:39:37 -0300

    Preparando os arquivos para aula

---
+++ modulo-02/30-aula-tipo-never/neverType.ts
@@ -0,0 +1,15 @@
+/**
+ * arquivo: neverType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'never'
+ * data: 08/10/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#never
+ */
+
+// ==> Exemplo 01: Never - Throw Exception
+
+// ==> Exemplo 02: Never inferido automaticamente
+
+// ==> Exemplo 03 - Função que contém loop infinito retorna o tipo 'never'
+
+// ==> Exemplo 04: Diferença entre os tipos: 'never' x 'void'
