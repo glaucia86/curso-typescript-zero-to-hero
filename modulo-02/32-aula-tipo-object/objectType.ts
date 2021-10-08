f664bd40a8ad2d9713dfe2d2a5bad80cb96c0c7a
glaucia86 <glaucia_lemos86@hotmail.com>
2021-10-07 22:43:31 -0300

    Preparando os arquivos para aula - 32

---
+++ modulo-02/32-aula-tipo-object/objectType.ts
@@ -0,0 +1,7 @@
+/**
+ * arquivo: objectType.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre o tipo 'object'
+ * data: 08/10/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ * doc referência: https://www.typescriptlang.org/docs/handbook/2/objects.html
+ */
