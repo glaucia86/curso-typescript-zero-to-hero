b881e588780f71a1033baf660070045e403533f7
glaucia86 <glaucia_lemos86@hotmail.com>
2021-12-06 20:51:01 -0300

    alterando o arquivo README.md do módulo 03

--- modulo-02/08-aula-type-annotation/typeAnnotation.ts
+++ modulo-02/08-aula-type-annotation/typeAnnotation.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * arquivo: typeAnnotation.ts
  * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Type Annotation'
@@ -7,6 +8,7 @@
  * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
  */
 
+export { };
 
 // ==> Variáveis [Type Annotations]
 let nome: string = 'Glaucia Lemos';
@@ -18,17 +20,17 @@ console.log(animais);
 
 // ==> Objetos [Type Annotations]
 let carro: {
-    nome: string;
-    ano: number;
-    preco: number
+  nome: string;
+  ano: number;
+  preco: number;
 };
 
-carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 }
+carro = { nome: 'Toyota Yaris Sedan XS', ano: 2019, preco: 80000 };
 console.log(carro);
 
 // ==> Functions [Type Annotation]
 function multiplicarNumero(num1: number, num2: number) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
-console.log(multiplicarNumero(2,5));
+console.log(multiplicarNumero(2, 5));
--- modulo-03/README.md
+++ modulo-03/README.md
@@ -1,14 +1,14 @@
-# Módulo 3: Fluxos de Controle  
+# Módulo 3: Fluxos de Controle
 
 Aqui vocês encontrarão todos os vídeos e os exemplos desenvolvidos durante o módulo 03.
 
 ## 💻 Série de Vídeos
 
-| Vídeo Aula | Descrição |
-|---|---|
-| **[Vídeo 01]()** | Vídeo 01 -  |
-| **[Vídeo 02]()** | Vídeo 02 -  |
-| **[Vídeo 03]()** | Vídeo 03 -  |
-| **[Vídeo 04]()** | Vídeo 04 -  |
-| **[Vídeo 05]()** | Vídeo 05 -  |
-| **[Vídeo 06]()** | Vídeo 06 -  |
+| Vídeo Aula       | Descrição  |
+| ---------------- | ---------- |
+| **[Vídeo 34]()** | Vídeo 01 - |
+| **[Vídeo 35]()** | Vídeo 02 - |
+| **[Vídeo 36]()** | Vídeo 03 - |
+| **[Vídeo 37]()** | Vídeo 04 - |
+| **[Vídeo 38]()** | Vídeo 05 - |
+| **[Vídeo 39]()** | Vídeo 06 - |
