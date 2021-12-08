94ccfca2de081dce8be2311025d1c564999eb1a6
glaucia86 <glaucia_lemos86@hotmail.com>
2021-12-07 21:47:30 -0300

    criando os exercícios para aula - 35

---
+++ modulo-03/aula-35-if-else/if-else.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+/**
+ * arquivo: ifElse.ts
+ * descrição: arquivo responsável por ensinar como usar instrução if-else no Typescript
+ * data: 07/12/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export { };
+
+// ==> Exemplo 01 - Uso do if
+
+// ==> Exemplo 02 - Uso do if
+
+// ==> Exemplo 03 - Uso do if-else
+
+//==> Exemplo 04 - if... else if (cálculo IMC - https://www.programasaudefacil.com.br/calculadora-de-imc)
+
+// ==> Exemplo 05 - Ternário (? :) - if...else
