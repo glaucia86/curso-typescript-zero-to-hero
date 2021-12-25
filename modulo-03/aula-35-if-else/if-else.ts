39572bd92b0c32a1c6f7e2549ba8c6c5739b9720
glaucia86 <glaucia_lemos86@hotmail.com>
2021-12-24 21:34:45 -0300

    adicionando os códigos exemplos 35

--- modulo-03/aula-35-if-else/if-else.ts
+++ modulo-03/aula-35-if-else/if-else.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 /* eslint-disable prettier/prettier */
 /**
  * arquivo: ifElse.ts
@@ -10,10 +11,62 @@ export { };
 
 // ==> Exemplo 01 - Uso do if
 
+const numeroMax = 100;
+let contador = 100;
+
+if (contador < numeroMax) {
+  contador++
+}
+
+console.log(contador);
+
 // ==> Exemplo 02 - Uso do if
+
+const permissaoIdadeDirigir = 16;
+
+if (permissaoIdadeDirigir >= 18) {
+  console.log('Você está habilitado para dirigir!')
+}
 
 // ==> Exemplo 03 - Uso do if-else
 
-//==> Exemplo 04 - if... else if (cálculo IMC - https://www.programasaudefacil.com.br/calculadora-de-imc)
+const permissaoIdadeDirigir01 = 18;
+
+if (permissaoIdadeDirigir01 <= 17) {
+  console.log('Você está habilitado para dirigir!')
+} else {
+  console.log('Você não está habilitado para dirigir!')
+}
+
+//==> Exemplo 04 - if... else if
+
+let desconto: number;
+
+let valorCompra = 14;
+
+if (valorCompra > 0 && valorCompra <= 5) {
+  desconto = 5;
+} else if (valorCompra > 5 && valorCompra <= 10) {
+  desconto = 10;
+} else {
+  desconto = 15;
+}
+
+console.log(`Você teve um desconto de...: ${desconto}% desconto`);
 
 // ==> Exemplo 05 - Ternário (? :) - if...else
+
+/*const idadeVotacao = 18;
+
+if (idadeVotacao >= 18) {
+  console.log('Você é elegível para votar.')
+} else {
+  console.log('Você não é elegível para votar.')
+}*/
+
+// ==> Ternário
+const idadeVotacao = 16;
+
+const podeVotar = (idadeVotacao >= 18) ? 'Você é elegível para votar.' : 'Você não é elegível para votar.'
+
+console.log(podeVotar)
