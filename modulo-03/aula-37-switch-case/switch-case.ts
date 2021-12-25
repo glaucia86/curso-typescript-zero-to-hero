73f28186eec8e91518960344657c0db11273e622
glaucia86 <glaucia_lemos86@hotmail.com>
2021-12-24 22:56:32 -0300

    exercícios 37

--- modulo-03/aula-37-switch-case/switch-case.ts
+++ modulo-03/aula-37-switch-case/switch-case.ts
@@ -11,4 +11,47 @@ export { };
 
 // ==> Exemplo 01 - Uso do switch...case
 
+let flor = 'tulipa';
+
+switch (flor) {
+  case 'rosa':
+    console.log('Rosas são vermelhas');
+    break;
+  case 'violeta':
+    console.log('Violetas são azuis');
+    break;
+  case 'tulipa':
+    console.log('Tulipas são brancas');
+    break;
+
+  default:
+    console.log('Por favor, selecione uma outra flor!');
+}
+
 // ==> Exemplo 02 - Uso do switch...case
+
+let diaUtilSemana = 5;
+
+switch (diaUtilSemana) {
+  case 0:
+    console.log('Domingo');
+    break;
+  case 1:
+    console.log('Segunda');
+    break;
+  case 2:
+    console.log('Terça');
+    break;
+  case 3:
+    console.log('Quarta');
+    break;
+  case 4:
+    console.log('Quinta');
+    break;
+  case 5:
+    console.log('Sexta');
+    break;
+
+  default:
+    console.log('Não é um dia útil');
+}
