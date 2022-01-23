5c258b2b8ec27fd30c04daa9a11de7019322b47a
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-23 20:17:15 -0300

    criando o exemplo - 39

---
+++ modulo-03/aula-39-loop-for/loop-for.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+/**
+ * arquivo: loop-for.ts
+ * descrição: arquivo responsável por ensinar como usar o loop for no Typescript
+ * data: 23/01/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+// ==> Exemplo 01: loop for
+
+// ==> Exemplo 02: for ...of - (retorna uma coleção de matriz, lista ou tupla)
+
+// ==> Exemplo 03: for ...in - ()
