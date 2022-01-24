4e3d6f5b85dc69a8634cba761fe9150e7331a15c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-23 21:06:35 -0300

    incluindo o exercicio - 41

---
+++ modulo-03/aula-41-loop-while/loop-while.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+/**
+ * arquivo: loop-while.ts
+ * descrição: arquivo responsável por ensinar como usar o loop for no Typescript
+ * data: 23/01/2021
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+// ==> Exemplo 01 - while
+
+// ==> Exemplo 02
+
+// ==> Exemplo 03 - exemplo mais prático
+
+// ==> Exemplo 04 - do...while
+
