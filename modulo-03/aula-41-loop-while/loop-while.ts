0b72d62751a41ba121fd7a90f88fd5154d49b769
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-24 22:56:43 -0300

    exercício aula 41

--- modulo-03/aula-41-loop-while/loop-while.ts
+++ modulo-03/aula-41-loop-while/loop-while.ts
@@ -7,10 +7,38 @@
  */
 
 // ==> Exemplo 01 - while
+let contador = 0;
+
+while (contador < 5) {
+  console.log(contador);
+  contador++;
+}
 
 // ==> Exemplo 02
+let numero = 1;
+
+while (numero <= 20) {
+  if (numero % 5 == 0) {
+    console.log('O primeiro número múltiplo de 5 entre 1 a 20 é...: ', numero);
+    break;
+  }
+  numero++
+}
 
 // ==> Exemplo 03 - exemplo mais prático
+let contadorUsuario = 0;
+const usuario : string[] = ['Glaucia', 'Jurema', 'Prince'];
+
+while (usuario[contadorUsuario]) {
+
+  console.log('Usuários...: ', usuario[contadorUsuario]);
+  contadorUsuario++;
+}
 
 // ==> Exemplo 04 - do...while
+let contador_01 = 0;
 
+do {
+  console.log(contador_01);
+  contador_01++
+} while (contador_01 < 5)
