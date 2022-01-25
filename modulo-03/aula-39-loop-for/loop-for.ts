fe193adaead83d072812257514680a3676869e69
glaucia86 <glaucia_lemos86@hotmail.com>
2022-01-24 22:14:16 -0300

    exercícios aula 39

--- modulo-03/aula-39-loop-for/loop-for.ts
+++ modulo-03/aula-39-loop-for/loop-for.ts
@@ -8,6 +8,25 @@
 
 // ==> Exemplo 01: loop for
 
-// ==> Exemplo 02: for ...of - (retorna uma coleção de matriz, lista ou tupla)
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
 
-// ==> Exemplo 03: for ...in - ()
+// ==> Exemplo 02: for ...of - (retorna uma coleção de matriz, lista ou tupla)
+const arrayNumeros = [10, 20, 30, 40, 50];
+
+for (const i of arrayNumeros) {
+  console.log(i);
+}
+
+// ==> Exemplo 03: for ...in - (retorna uma coleção de matriz, lista ou tupla)
+
+const arrayNumeros_01 = [5, 4, 3, 2, 1, 0]
+
+// for (const i in arrayNumeros_01) {
+//   console.log(i);
+// }
+
+// sort()
+const ordemNumerica: number[] = arrayNumeros_01.sort();
+console.log(ordemNumerica);
