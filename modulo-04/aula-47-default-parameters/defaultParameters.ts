fbcb85bca57d0c156d0dc2386d7cdbe16125d8db
glaucia86 <glaucia_lemos86@hotmail.com>
2022-03-04 18:23:31 -0300

    criando as pastas para os exercícios

---
+++ modulo-04/aula-47-default-parameters/defaultParameters.ts
@@ -0,0 +1,15 @@
+/**
+ * arquivo: defaultParameters.ts
+ * descrição: arquivo responsável por ensinar como usar o
+ * 'Default Parameters' em funções no no Typescript
+ * data: 22/02/2022
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export {};
+
+// ==> Exemplo 01 - Default Parameters
+
+// ==> Exemplo 02
+
+// ==> Exemplo 03
---
+++ modulo-04/aula-49-rest-parameters/restParameters.ts
@@ -0,0 +1,15 @@
+/**
+ * arquivo: restParameters.ts
+ * descrição: arquivo responsável por ensinar como usar o
+ * 'Rest Parameters' em funções no no Typescript
+ * data: 06/03/2022
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export {};
+
+// ==> Exemplo 01
+
+// ==> Exemplo 02
+
+// ==> Exemplo 03
