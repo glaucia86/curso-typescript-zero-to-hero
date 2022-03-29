74cc8c897c9816119624fa06fabdc6f9421a621b
glaucia86 <glaucia_lemos86@hotmail.com>
2022-03-29 20:10:04 -0300

    desenvolvimento do exemplo da aula 47

--- modulo-04/aula-47-default-parameters/defaultParameters.ts
+++ modulo-04/aula-47-default-parameters/defaultParameters.ts
@@ -2,14 +2,37 @@
  * arquivo: defaultParameters.ts
  * descrição: arquivo responsável por ensinar como usar o
  * 'Default Parameters' em funções no no Typescript
- * data: 22/02/2022
+ * data: 29/03/2022
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
 export {};
 
 // ==> Exemplo 01 - Default Parameters
+function descontoCompra(preco: number, desconto = 0.08) {
+  return preco * (1 - desconto);
+}
+
+console.log(descontoCompra(100)); //92
 
 // ==> Exemplo 02
+function exibirMensagem(mensagem: string, saudar = 'Fala, pessoal!') {
+  return saudar + ' ' + mensagem + '!';
+}
+
+console.log(exibirMensagem('JavaScript Developers'));
 
 // ==> Exemplo 03
+function exibirNome(nome: string, sobrenome = 'Lemos') {
+  return nome + ' ' + sobrenome;
+}
+
+const resultado_1 = exibirNome('Glaucia');
+const resultado_2 = exibirNome('Glaucia', undefined);
+// const resultado_3 = exibirNome('Glaucia', 'de Souza', 'Senhorita');
+const resultado_4 = exibirNome('Glaucia', 'de Souza');
+
+console.log(resultado_1);
+console.log(resultado_2);
+// console.log(resultado_3);
+console.log(resultado_4);
