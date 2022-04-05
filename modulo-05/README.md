0f17ff61e7c7af5bf6f66d24af5de9224e8c1efe
glaucia86 <glaucia_lemos86@hotmail.com>
2022-04-05 10:51:34 -0300

    enviando o exercício 51

--- modulo-05/README.md
+++ modulo-05/README.md
@@ -6,8 +6,8 @@ Aqui vocês encontrarão todos os vídeos e os exemplos desenvolvidos durante o 
 
 | Vídeo Aula | Descrição |
 |---|---|
-| **[Vídeo 50]()** | Vídeo 50 -  |
-| **[Vídeo 51]()** | Vídeo 51 -  |
+| **[Vídeo 50](https://youtu.be/7W3PNpoCB40)** | Vídeo 50 - Intro à Classes |
+| **[Vídeo 51](https://youtu.be/tKVsXBMYm1g)** | Vídeo 51 - Demo: Classes|
 | **[Vídeo 52]()** | Vídeo 52 -  |
 | **[Vídeo 53]()** | Vídeo 53 -  |
 | **[Vídeo 54]()** | Vídeo 54 -  |
--- modulo-05/aula-51-classes/classes.ts
+++ modulo-05/aula-51-classes/classes.ts
@@ -5,3 +5,68 @@
  * data: 03/04/2022
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
+
+// ==> Exemplo 01 - Classes
+
+export {};
+
+class Pessoa {
+  nome: string;
+  sobrenome: string;
+
+  constructor(nome: string, sobrenome: string) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+  }
+
+  nomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+}
+
+const pessoa = new Pessoa('Glaucia', 'Lemos');
+console.log(pessoa.nomeCompleto());
+
+// ==> Exemplo 02 - Classes (sem constructor)
+class Estudante {
+  codigoEstudante: number;
+  nomeEstudante: string;
+}
+
+// Criar um objeto ou a instancia
+const estudante = new Estudante();
+
+// Inicializar o objeto:
+estudante.codigoEstudante = 8967;
+estudante.nomeEstudante = 'Prince Lemos';
+
+// Acessar os campos:
+console.log('Código do Estudante...: ' + estudante.codigoEstudante);
+console.log('Nome do Estudante...: ' + estudante.nomeEstudante);
+
+// ==> Exemplo 03 - Classes (com constructor)
+class Estudante_1 {
+  codigoEstudante: number;
+  nomeEstudante: string;
+
+  // Definir o Construtor
+  constructor(codigoEstudante: number, nomeEstudante: string) {
+    this.codigoEstudante = codigoEstudante;
+    this.nomeEstudante = nomeEstudante;
+  }
+
+  // Criar o método
+  listarEstudante(): void {
+    console.log('Código do Estudante...: ' + this.codigoEstudante);
+    console.log('Nome do Estudante...: ' + this.nomeEstudante);
+  }
+}
+
+// Acessar os campos:
+const estudante_1 = new Estudante_1(9845, 'Prince Lemos');
+console.log(
+  'Lendo o atributo Código do Estudante...: ' + estudante_1.codigoEstudante,
+);
+console.log(
+  'Lendo o atributo Nome do Estudante...: ' + estudante_1.nomeEstudante,
+);
