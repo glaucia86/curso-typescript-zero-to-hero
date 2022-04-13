791d3420382f41523aa6e2fea826d0f3405e77c0
glaucia86 <glaucia_lemos86@hotmail.com>
2022-04-13 15:05:50 -0300

    Incluindo exercício 53 - modificadores de acesso

--- modulo-05/aula-53-access-modifier/accessModifier.ts
+++ modulo-05/aula-53-access-modifier/accessModifier.ts
@@ -9,11 +9,65 @@
 export {};
 
 // ==> Exemplo 01- Modificador: public
+class Estudante {
+  codigoEstudante: number;
+  nomeEstudante: string;
+}
+
+const estudante = new Estudante();
+estudante.codigoEstudante = 201;
+estudante.nomeEstudante = 'Glaucia Lemos';
+
+console.log(estudante.codigoEstudante);
+console.log(estudante.nomeEstudante);
 
 // ==> Exemplo 02 - Modificador: private
+class Estudante_02 {
+  codigoEstudante: number;
+  nomeEstudante: string;
+  private idade: number;
 
-// ==> Exemplo 03 - Modificador: private
+  constructor(codigEstudante: number, nomeEstudante: string, idade: number) {
+    this.codigoEstudante = codigEstudante;
+    this.nomeEstudante = nomeEstudante;
+    this.idade = idade;
+  }
 
-// ==> Exemplo 04 - Modificador: protected
+  retornarDadosEstudante() {
+    return `Código do Aluno...: ${this.codigoEstudante}.
+      Nome do Estudante...: ${this.nomeEstudante}.
+      Idade do Estudante...: ${this.idade}`;
+  }
+}
 
-// ==> Exemplo 04 - Modificador: readonly
+const estudante_02 = new Estudante_02(98765, 'Glaucia Lemos', 35);
+console.log(estudante_02.retornarDadosEstudante());
+
+// ==> Exemplo 03 - Modificador: protected
+class Estudante_03 {
+  codigoEstudante: number;
+  protected nomeEstudante: string;
+
+  constructor(codigoEstudante: number, nomeEstudante: string) {
+    this.codigoEstudante = codigoEstudante;
+    this.nomeEstudante = nomeEstudante;
+  }
+}
+
+class Pessoa extends Estudante_03 {
+  private curso: string;
+
+  constructor(codigoEstudante: number, nomeEstudante: string, curso: string) {
+    super(codigoEstudante, nomeEstudante);
+    this.curso = curso;
+  }
+
+  retornarDados() {
+    return `Código do Aluno: ${this.codigoEstudante}.
+    Nome do Estudante: ${this.nomeEstudante}.
+    Matéria: ${this.curso}`;
+  }
+}
+
+const estudante_03 = new Pessoa(783212, 'Glaucia Lemos', 'Matemática');
+console.log(estudante_03.retornarDados());
