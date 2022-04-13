847a53506ee3ce7db7c5bf50bd1c19cf3e2584f8
glaucia86 <glaucia_lemos86@hotmail.com>
2022-04-12 21:25:29 -0300

    atualizando o arquivo README.md - módulo 05

---
+++ modulo-05/aula-53-access-modifier/accessModifier.ts
@@ -0,0 +1,19 @@
+/**
+ * arquivo: accessModifier.ts
+ * descrição: arquivo responsável por ensinar como usar o 'Modificadores de
+ * Acesso' no Typescript
+ * data: 12/04/2022
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+export {};
+
+// ==> Exemplo 01- Modificador: public
+
+// ==> Exemplo 02 - Modificador: private
+
+// ==> Exemplo 03 - Modificador: private
+
+// ==> Exemplo 04 - Modificador: protected
+
+// ==> Exemplo 04 - Modificador: readonly
