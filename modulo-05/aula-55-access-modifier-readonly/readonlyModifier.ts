1c8d274e1f8d2455aea4ee0326ab531f7f0bb586
glaucia86 <glaucia_lemos86@hotmail.com>
2022-07-14 11:18:52 -0300

    adicionando os exemplos vídeo -55

--- modulo-05/aula-55-access-modifier-readonly/readonlyModifier.ts
+++ modulo-05/aula-55-access-modifier-readonly/readonlyModifier.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * arquivo: readonlyModifier.ts
  * descrição: arquivo responsável por ensinar como usar o 'Modificadores de
@@ -8,10 +9,63 @@
 
 export { };
 
-// ==> Exemplo 01 - Modificador: readonly
+// ==> Exemplo 01
+class Funcionario {
+  readonly dataNascimento: Date;
 
-// ==> Exemplo 02 - Com acesso:
+  constructor(dataNascimento: Date) {
+    this.dataNascimento = dataNascimento;
+  }
+}
+
+const funcionario = new Funcionario(new Date(1986, 10, 24));
+// funcionario.dataNascimento = new Date(1986, 10, 24);
+
+// ==> Exemplo 02
+
+class Funcionario_01 {
+  constructor(readonly dataNascimento: Date) {
+    this.dataNascimento = dataNascimento;
+  }
+}
 
 // ==> Exemplo 03
 
+class Funcionario_03 {
+  nome: string;
+  readonly codigoFuncionario: number;
+
+  constructor(nome: string, codigo: number) {
+    this.codigoFuncionario = codigo;
+    this.nome = nome;
+  }
+}
+
+const func = new Funcionario_03('Glaucia', 112233);
+func.nome = 'Glaucia Lemos';
+// func.codigoFuncionario = 113344;
+
+console.log(func);
+
 // ==> Exemplo 04 - Interface
+
+interface IFuncionario {
+  codigoFuncionario: number;
+  nomeEmpregado: string;
+}
+
+const funcionario_01: Readonly<IFuncionario> = {
+  codigoFuncionario: 506699,
+  nomeEmpregado: 'Jurema',
+};
+
+// funcionario_01.codigoFuncionario = 887653;
+// funcionario_01.nomeEmpregado = 'Lemos';
+
+const funcionario_02: IFuncionario = {
+  codigoFuncionario: 506699,
+  nomeEmpregado: 'Jurema',
+};
+
+funcionario_02.codigoFuncionario = 887653;
+funcionario_02.nomeEmpregado = 'Lemos';
