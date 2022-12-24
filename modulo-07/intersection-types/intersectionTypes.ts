bfc58c66f4d29f02a487d19efd18a7d2d696c197
glaucia86 <glaucia_lemos86@hotmail.com>
2022-12-24 20:42:59 -0300

    feat: incluindo code sample aula 68

--- modulo-07/intersection-types/intersectionTypes.ts
+++ modulo-07/intersection-types/intersectionTypes.ts
@@ -7,8 +7,59 @@
 
 export = {};
 
-// ==> Exemplo 01 - Intersection TypesIntersection Types:
+// ==> Exemplo 01 - Intersection Types:
 
+interface DadosBancarios {
+  conta: number;
+  agencia: number;
+  banco: string;
+}
 
+interface Cliente {
+  nome: string;
+  email: string;
+}
+
+interface DadosPessoaFisica {
+  cpf: number;
+}
+
+type DadosCliente = DadosBancarios & Cliente & DadosPessoaFisica;
+
+const dadosCliente: DadosCliente = {
+  conta: 123456,
+  agencia: 123,
+  banco: "Banco do Brasil",
+  nome: "Glaucia Lemos",
+  email: "glaucia@email.com",
+  cpf: 123456789,
+}
+
+console.log(dadosCliente)
 
 // ==> Exemplo 02 - Intersection Types:
+
+interface IEndereco {
+  rua: string;
+  bairro: string;
+  cidade: string;
+}
+
+interface IPessoa {
+  nome: string;
+  idade: number;
+  profissao: string;
+}
+
+type IEnderecoPessoa = IEndereco & IPessoa;
+
+const enderecoPessoa: IEnderecoPessoa = {
+  nome: 'Glaucia Lemos',
+  idade: 36,
+  profissao: 'Software Engineer',
+  rua: 'Rua das Tulipas',
+  bairro: 'Bairro das Tulipas',
+  cidade: 'Rio de Janeiro',
+};
+
+console.log(enderecoPessoa);
