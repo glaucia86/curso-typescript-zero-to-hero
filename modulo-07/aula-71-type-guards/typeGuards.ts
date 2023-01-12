d5c48ae7f67c662d7b94ca5767fe20bd2671d5d5
glaucia86 <glaucia_lemos86@hotmail.com>
2023-01-12 20:03:20 -0300

    atualizando as demonstrações

---
+++ modulo-07/aula-71-type-guards/typeGuards.ts
@@ -0,0 +1,16 @@
+/**
+ * arquivo: typeGuards.ts
+ * descrição: arquivo responsável por ensinar o uso de Type Guards em TypeScript
+ * data: 01/12/2022
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+exports = {};
+
+// ==> Exemplo 01 - Type Guards: typeof
+
+
+// ==> Exemplo 02 - Type Guards: instanceof
+
+
+// ==> Exemplo 03 - Type Guards: in
