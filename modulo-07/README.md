2bfcf30cb1bef5af263b1b3192e497ae9de2bc65
glaucia86 <glaucia_lemos86@hotmail.com>
2023-01-12 22:29:19 -0300

    enviando os exemplos da aula 71

--- modulo-07/README.md
+++ modulo-07/README.md
@@ -4,11 +4,11 @@ Aqui vocês encontrarão todos os vídeos e os exemplos desenvolvidos durante o 
 
 ## 💻 Série de Vídeos
 
-| Vídeo Aula       | Descrição  |
-| ---------------- | ---------- |
+| Vídeo Aula                                   | Descrição                     |
+| -------------------------------------------- | ----------------------------- |
 | **[Vídeo 68](https://youtu.be/2hE57wR86YY)** | Vídeo 68 - Intersection Types |
 | **[Vídeo 69](https://youtu.be/LvVWaHk3l0I)** | Vídeo 69 - Intersection Types |
-| **[Vídeo 70]()** | Vídeo 70 - |
-| **[Vídeo 71]()** | Vídeo 71 - |
-| **[Vídeo 72]()** | Vídeo 72 - |
-| **[Vídeo 73]()** | Vídeo 73 - |
+| **[Vídeo 70](https://youtu.be/DNmCS4PT9bc)** | Vídeo 70 - Type Guards        |
+| **[Vídeo 71](https://youtu.be/83eUc_Qzvxk)** | Vídeo 71 - Demo- Type Guards  |
+| **[Vídeo 72]()**                             | Vídeo 72 -                    |
+| **[Vídeo 73]()**                             | Vídeo 73 -                    |
--- modulo-07/aula-71-type-guards/typeGuards.ts
+++ modulo-07/aula-71-type-guards/typeGuards.ts
@@ -8,9 +8,100 @@
 exports = {};
 
 // ==> Exemplo 01 - Type Guards: typeof
+type alfanumerico = string | number;
 
+function exibirTipo(a: alfanumerico, b: alfanumerico) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+
+  if (typeof a === 'string' && typeof b === 'string') {
+    return a.concat(b);
+  }
+
+  throw new Error('Argumentos inválidos! Ambos os argumentos devem ser númericos ou string.');
+}
+
+console.log(exibirTipo('Glaucia', '2'));
+console.log(exibirTipo(5, 5));
+// console.log(exibirTipo('Lemos', 5));
 
 // ==> Exemplo 02 - Type Guards: instanceof
+class Carro {
+  nome: string;
+  marca: string;
+
+  constructor(nome: string, marca: string) {
+    this.nome = nome;
+    this.marca = marca;
+  }
+}
+
+class Moto {
+  nome: string;
+  ano: number;
+
+  constructor(nome: string, ano: number) {
+    this.nome = nome;
+    this.ano = ano;
+  }
+}
+
+function detalhesVeiculo(veiculo: Carro | Moto) {
+  if (veiculo instanceof Carro) {
+    return `O nome do carro é: ${veiculo.nome} e a marca é: ${veiculo.marca}`;
+  } else if (veiculo instanceof Moto) {
+    return `O nome da moto é: ${veiculo.nome} e o ano é: ${veiculo.ano}`;
+  }
+}
+
+const carro = new Carro('Gol', 'Volkswagen');
+console.log(detalhesVeiculo(carro));
+
+const moto = new Moto('CBR', 2020);
+console.log(detalhesVeiculo(moto));
 
 
 // ==> Exemplo 03 - Type Guards: in
+interface Animal_05 {
+  grupo: string;
+}
+
+class Peixe implements Animal_05 {
+  grupo: string;
+  corPeixe: string;
+
+  constructor(grupo: string, corPeixe: string) {
+    this.grupo = grupo;
+    this.corPeixe = corPeixe;
+  }
+}
+
+class Passaro implements Animal_05 {
+  grupo: string;
+  corPena: string;
+
+  constructor(grupo: string, corPena: string) {
+    this.grupo = grupo;
+    this.corPena = corPena;
+  }
+}
+
+function nadar(grupo: string) {
+  console.log(`O ${grupo} está nadando!`);
+}
+
+function voar(grupo: string) {
+  console.log(`O ${grupo} está voando!`);
+}
+
+function mover(animal: Animal_05) {
+  if ('corPeixe' in animal) {
+    nadar((animal as Peixe).grupo);
+  } else if ('corPena' in animal) {
+    voar((animal as Passaro).grupo);
+  }
+}
+
+mover(new Passaro('Pássaro', 'Vermelho'));
+mover(new Peixe('Peixe', 'Azul'));
