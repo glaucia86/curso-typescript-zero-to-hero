e4497efd83a70893108d19d11111904632493799
glaucia86 <glaucia_lemos86@hotmail.com>
2023-05-08 18:58:48 -0300

    chore: sample class 90

---
+++ modulo-09/aula-91-intro-modules/main.ts
@@ -0,0 +1,13 @@
+/**
+ * arquivo: main.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Modules'
+ * data: 05/08/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { somar, subtrair, multiplicar, dividir } from './math';
+
+console.log(somar(10, 20));
+console.log(subtrair(10, 20));
+console.log(multiplicar(10, 20));
+console.log(dividir(10, 20));
---
+++ modulo-09/aula-91-intro-modules/math.ts
@@ -0,0 +1,28 @@
+/**
+ * arquivo: math.ts
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Modules'
+ * data: 05/08/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+// ==> Exemplo 01: Modules
+
+export function somar(x: number, y: number): number {
+  return x + y;
+}
+
+export function subtrair(x: number, y: number): number {
+  return x - y;
+}
+
+export function multiplicar(x: number, y: number): number {
+  return x * y;
+}
+
+export function dividir(x: number, y: number): number {
+  return x / y;
+}
+
+
+
+
