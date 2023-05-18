317d57f8eeb505c6ecf06bdac11946fc612b8ffd
glaucia86 <glaucia_lemos86@hotmail.com>
2023-05-18 14:15:12 -0300

    chore: configure scripts to run the app

---
+++ modulo-10/ts-node-express/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * arquivo: index.ts
+ * descrição: arquivo responsável por fazer a chamada da api do Express.js
+ * data: 05/18/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+console.log('Oi, Developers!')
