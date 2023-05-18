74d118e4db1824de96385b40081b123223a13b8c
glaucia86 <glaucia_lemos86@hotmail.com>
2023-05-18 14:40:46 -0300

    chore: create api using Express.js

--- modulo-10/ts-node-express/src/index.ts
+++ modulo-10/ts-node-express/src/index.ts
@@ -5,4 +5,21 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-console.log('Oi, Developers!')
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello, Developers! Sejam bem-vindos(as) ao TypeScript com Express.Js!</h1>');
+});
+
+app.listen(PORT, () => console.log(`Servidor executando na porta ${PORT} `));
